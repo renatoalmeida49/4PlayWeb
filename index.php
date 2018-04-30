8c048bd5fb48138db75d13a132ddc9b98e1928ed
novaes <renatoalmeida49@gmail.com>
2018-04-30 15:48:33 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		<title>4Play</title>
+	</head>
+	<body>
+		<div class="main">
+			<form method="POST">
+				Login:
+				<br><input type="text" name="nome"><br>
+				<br>Senha:
+				<br><input type="password" name="senha"><br>
+
+				<br><input type="submit" value="Enviar">
+			</form>
+		</div>
+	</body>
+</html>
