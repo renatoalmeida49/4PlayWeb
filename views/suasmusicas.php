6cdd7e374014e795d0493529e0081ba5c86ed875
novaes <renatoalmeida49@gmail.com>
2018-05-31 09:07:58 -0300

    Continuidade no desenvolvimento. Das telas do sistema.

--- views/suasmusicas.php
+++ views/suasmusicas.php
@@ -1,4 +1,44 @@
-<?php
-
-echo "suas musicas";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="../assets/css/styleSuasMusicas.css"/>
+        <link rel="icon" type="image/png" href="../assets/images/logo.png"/>
+        <title>Suas músicas</title>
+    </head>
+    
+    <body>
+        <header></header>
+        
+        <section>
+            <div class="tabelaSuasMusicas">
+                <table>
+                    <tr>
+                        <td>Nome</td>
+                        <td>Artista</td>
+                        <td>Tipo</td>
+                        <td>Capo</td>
+                        <td>Idioma</td>
+                        <td>Instrumento</td>
+                    </tr>
+                </table>
+            </div>
+            
+            <div class="infoMusica">
+                Nome:<br/>
+                Artista:<br/>
+                Tipo:<br/>
+                Capo:<br/>
+                Idioma:<br/>
+                Instrumento:<br/>
+            </div>
+            
+            <div class="letra">
+                A letra da música
+            </div>
+        </section>
+        
+        <footer></footer>
+    </body>
+</html>
 
