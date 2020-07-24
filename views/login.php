8b5c18607d791c65a0bf40c5209ecf5b4cf2bc76
Renato Novaes <renatoalmeida49@gmail.com>
2020-07-24 17:13:14 -0300

    Desenvolvendo nova interface para login no sistema. Problema do BD ainda persiste

--- views/login.php
+++ views/login.php
@@ -1,45 +1,46 @@
 <!DOCTYPE html>
 <html>
     <head>
-	<meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, inital-scale=1, shrink-to-fit=no" />
-        <link rel="icon" type="imagem/png" href="<?php echo BASE_URL; ?>assets/images/logo.png" />
-        <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css" />
-        <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/script/bootstrap.min.js"></script>
-	<title>4Play</title>
+	   <meta charset="utf-8" />
+       <meta name="viewport" content="width=device-width, inital-scale=1, shrink-to-fit=no" />
+       <link rel="icon" type="imagem/png" href="<?php echo BASE_URL; ?>assets/images/logo.png" />
+       <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css" />
+       <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/styleLogin.css"/>
+	   <title>4Play</title>
     </head>
     
-    <body>
-        <div class="container">
-            <div class="row justify-content-center">
-                <h1>4Play</h1>
-            </div>
-            
-            <hr/>
-            
-            <div class="row justify-content-center">
-                <img src="<?php echo BASE_URL; ?>assets/images/logo.png" class="img-fluid"/>
-            </div>
-            
-            <hr/>
-            
-            <div class="row justify-content-center">
-                <form method="post">
-                    <div class="form-group">
-                        <label for="login">Login:</label><input autofocus required id="login" class="form-control" type="text" name="login"/></br>
-                        <label for="password">Senha:</label><input required id="password" class="form-control" type="password" size="32" name="password"/></br>
-                    </div>
+    <body class="text-center">
+        <form class="form-singin" method="POST">
+            <img src="<?php echo BASE_URL; ?>assets/images/logo.png" class="mb-4" width="90" height="90"/>
+            <h1 class="mb-3">4Play</h1>
+            <label for="login" class="sr-only">Login</label>
+            <input autofocus required id="login" class="form-control" type="text" name="login" placeholder="Login" />
+            <label for="password" class="sr-only">Senha</label>
+            <input required id="password" class="form-control" type="password" size="32" name="password" placeholder="Senha" />
 
-                    <div class="form-row">
-                        <div class="col">
-                            <button class="btn btn-success form-control" style="margin-bottom: 5px" id="botao" type="submit" value="Acessar" name="userControl">Acessar</button>
+            <button class="btn btn-block btn-lg mb-3 btn-success margin" type="submit">Acessar</button>
+
+            <a href="#modalCadastro" class="btn btn-primary" data-toggle="modal" data-target="#modalCadastro">Criar login</a>
+        </form>
+
+        <div id="modalCadastro" class="modal fade">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <form method="POST">
+                        <div class="modal-header">
+                            <h2>Cadastro</h2>
+                            <button class="close" data-dismiss="modal"><span>&times;</span></button>
                         </div>
-                        <div class="col">
-                            <a href="views/cadastro.php" class="btn btn-success form-control">Criar login</a>
+                        <div class="modal-body">
                         </div>
-                    </div>
-                </form>
+                        <div class="modal-footer">
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
+
+        <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/script/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/script/bootstrap.min.js"></script>
     </body>
 </html>
--- views/template.php
+++ views/template.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
     <head>
-	<meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, inital-scale=1, shrink-to-fit=no" />
-        <link rel="icon" type="imagem/png" href="<?php echo BASE_URL; ?>assets/images/logo.png" />
-        <!-- <link rel="stylesheet" type="text/css" href="../assets/css/styleTelaInicial.css"> -->
-        <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css" />
-        <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/script/bootstrap.min.js"></script>
-	<title>Tela inicial</title>
+	   <meta charset="utf-8" />
+       <meta name="viewport" content="width=device-width, inital-scale=1, shrink-to-fit=no" />
+       <link rel="icon" type="imagem/png" href="<?php echo BASE_URL; ?>assets/images/logo.png" />
+       <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css" />
+	   <title>Tela inicial</title>
     </head>
       <body>
 
     <?php $this->loadViewInTemplate($viewName, $viewData); ?>
+
+    <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/script/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/script/bootstrap.min.js"></script>
 
     </body>
 </html>
