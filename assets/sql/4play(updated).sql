f86d6a469363e0a8fa4bd276aa598d96d01683fc
Renato Novaes <renatoalmeida49@gmail.com>
2020-07-29 08:11:10 -0300

    Banco de dados corrigido

--- assets/sql/4play(updated).sql
+++ assets/sql/4play(updated).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 27-Jul-2020 às 17:54
+-- Tempo de geração: 29-Jul-2020 às 11:09
 -- Versão do servidor: 10.4.10-MariaDB
 -- versão do PHP: 7.3.12
 
